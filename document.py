8a1ae6c28993d0e040f377923d9a3a8c24adf8d9
Raghu <raghu15sep99@gmail.com>
2019-11-05 17:44:07 +0530

    Converted tabs to spaces

--- document.py
+++ document.py
@@ -1,19 +1,19 @@
 class DocumentCollection:
-	def __init__(self):
-		# Mapping from document names to ids
-		self.dntid = {}
-		# Mapping from document ids to names
-		self.idtdn = []
-		# Number of documents in the collection
-		self.cnt = 0
+    def __init__(self):
+        # Mapping from document names to ids
+        self.dntid = {}
+        # Mapping from document ids to names
+        self.idtdn = []
+        # Number of documents in the collection
+        self.cnt = 0
 
-	def insert_document(self, document):
-		self.cnt += 1
-		self.dntid[document.name] = document.docid
-		self.idtdn.append(document.name) 
+    def insert_document(self, document):
+        self.cnt += 1
+        self.dntid[document.name] = document.docid
+        self.idtdn.append(document.name) 
 
 
 class Document:
-	def __init__(self, docid, name):
-		self.docid = docid
-		self.name = name
+    def __init__(self, docid, name):
+        self.docid = docid
+        self.name = name
--- minhasher.py
+++ minhasher.py
@@ -2,43 +2,43 @@ import numpy as np
 
 class MinHasher:
 
-	def __init__(self, n, d, shingler):
-		'''
-		n: number of hash functions
-		m: number of shingles
-		sig: signature matrix
-		d: number of documents
-		'''
-		self.shingler = shingler
-		m = len(shingler.cm)
-		self.m = m
-		self.n = n
-		self.d = d
-		self.hasher = Hasher(n, m)
-		# Here m is same as Infinity since all hash values are going to be less than m
-		self.sig = np.full(shape=(n, d), fill_value=m)
+    def __init__(self, n, d, shingler):
+        '''
+        n: number of hash functions
+        m: number of shingles
+        sig: signature matrix
+        d: number of documents
+        '''
+        self.shingler = shingler
+        m = len(shingler.cm)
+        self.m = m
+        self.n = n
+        self.d = d
+        self.hasher = Hasher(n, m)
+        # Here m is same as Infinity since all hash values are going to be less than m
+        self.sig = np.full(shape=(n, d), fill_value=m)
 
-		# Compute the signature matrix 
-		for j, s in enumerate(self.shingler.cm):
-			for docid in self.shingler.cm[s]:
-				for i in range(n):
-					h = self.hasher.hash(i, j)
-					self.sig[i][docid] = min(self.sig[i][docid], h)
+        # Compute the signature matrix 
+        for j, s in enumerate(self.shingler.cm):
+            for docid in self.shingler.cm[s]:
+                for i in range(n):
+                    h = self.hasher.hash(i, j)
+                    self.sig[i][docid] = min(self.sig[i][docid], h)
 
 class Hasher:
 
-	def __init__(self, n, m):
-		'''
-		Generates array of hash functions of the form a * x + b
-		n: number of hash functions
-		m: number of shingles
-		'''
-		self.a = np.random.randint(1, 100, n, dtype=int)
-		self.b = np.random.randint(1, 100, n, dtype=int)
-		self.m = m		
-	
-	def hash(self, i, x):
-		'''
-		Returns hash of x using the ith hash function
-		'''
-		return (self.a[i] * x + self.b[i]) % self.m
+    def __init__(self, n, m):
+        '''
+        Generates array of hash functions of the form a * x + b
+        n: number of hash functions
+        m: number of shingles
+        '''
+        self.a = np.random.randint(1, 100, n, dtype=int)
+        self.b = np.random.randint(1, 100, n, dtype=int)
+        self.m = m		
+    
+    def hash(self, i, x):
+        '''
+        Returns hash of x using the ith hash function
+        '''
+        return (self.a[i] * x + self.b[i]) % self.m
